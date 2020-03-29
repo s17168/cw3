2d3ed2dd8b903a3cfd9b6efe147ce61f7a0d231f
s17168 <s17168@pjwstk.edu.pl>
2020-03-29 15:49:24 +0200

    Create post method

--- Cw3WebApplication/Cw3WebApplication/Controllers/StudentsController.cs
+++ Cw3WebApplication/Cw3WebApplication/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cw3WebApplication.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -26,9 +27,17 @@ namespace Cw3WebApplication.Controllers
         }
 
         [HttpGet]
-        public string GetStudents(string orderBy) 
+        public string GetStudents([FromQuery] string orderBy) 
         {
             return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student) 
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 10000)}";
+
+            return Ok(student);
         }
 
     }
